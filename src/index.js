b58def5c00bde57426a5e19239bfdbeed8968bf2
lumeiling <1520622848@qq.com>
2020-04-09 14:10:19 +0800

    feat(highlight the winner)

--- src/index.js
+++ src/index.js
@@ -115,6 +115,7 @@ class Game extends React.Component {
       let status;
       if(winner) {
         status = 'Winner: ' + winner
+        getThreeSpots(current.squares)
       }else{
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
       }
@@ -165,10 +166,36 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-          // TODO a, b, c are supposed to be highlighted
           return squares[a];
         }
     }
     return null;
+}
+
+function getThreeSpots(squares){
+  const lines = [
+    // These are tripple line for winning
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        const buttonList = document.querySelectorAll("button.square")
+        for(let i = 0; i < buttonList.length; i++){
+          if(i === a || i === b | i === c){
+            debugger
+            buttonList[i].style.color = "hotPink"
+          }
+        }
+      }
+  }
+  return [];
 }
   
--- src/intro.js
+++ src/intro.js
@@ -9,6 +9,9 @@ const Intro = () => {
         Maybe this isn't a million dollar question, but it's fun.
         <ol>
           <li>Complete requirments</li>
+          <ol>
+            <li>When someone wins, highlight the three squares that caused the win.</li>
+          </ol>
           <li>Make it more interesting</li>
         </ol>
       </div>
