3ab2e97e5fe7b61e5bc6b5b722b679c18222c4c0
lumeiling <1520622848@qq.com>
2020-04-12 20:06:17 +0800

    feat(fix footer on the bottom)

--- src/footnote.js
+++ src/footnote.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const FootNote = () => {
     return (
-        <div className="foot-note">
+        <footer className="foot-note">
             <p>This is game demo</p>
             <p>{new Date().toLocaleString()}</p>
-        </div>
+        </footer>
     )
 }
 
